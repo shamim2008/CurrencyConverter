0016d0f53c22992cd56eafd7986dafb203ac52c4
shamim2008 <shamim08@gmail.com>
2017-11-19 17:09:42 +0000

    Importing File

---
+++ CurrencyConversion/CurrencyRates.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyConversion
+{
+    /// <summary>
+    ///This CurrencyRates class get a feed from API source. here I have created three Properties to store API record. These three properties (Parameter) does match with the API source key. Below I have given a short description about three properties.
+    /// </summary>
+    ///<param name="Base"> Receive base currency information from API</param>
+    /// <param name="Date"> Date  Parameter receive date information. This is the updated date for curency.  </param>
+    /// <param name="Rates"> Rates is important parameter here. I have store rates as dictionary. </param>
+    public class CurrencyRates
+    {
+        public string Base { get; set; }
+        public DateTime Date { get; set; }
+        public Dictionary<string, decimal> Rates { get; set; }
+    }
+
+
+    /// <summary>
+    /// CurrencyList class facilitate to store suppoted currency in the drop down. I have loaded those currency from CountryList.json file. More currency can be manually added to that file.
+    /// </summary>
+    /// <param name="Currency"> Currency properties will store all the key and value from CountryList.json file</param>
+    public class CurrencyList
+    {
+        public Dictionary<string,string> Currency { get; set; }
+    }
+}
---
+++ CurrencyConversion/MainWindow.xaml.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+using Newtonsoft.Json.Linq;
+
+namespace CurrencyConversion
+{
+    /// <summary>
+    /// <Author> Shamim Hossain</Author>
+    /// <Version> 1.0 </Version>
+    /// <Project> Currency Converter </Project> 
+    /// </summary>
+    /// 
+    public partial class MainWindow : Window
+    {
+
+        private string currentCurrency = string.Empty;
+        private string newCurrency = string.Empty;
+        
+        private const string JCurrencyList = "CurrencyList.json";
+        public MainWindow()
+        {
+            InitializeComponent();
+            LoadSuportedCurrency();
+        }
+
+        private string GetJasonObjectFromUrl(string url)
+        {
+            string jason;
+            using (WebClient wc = new WebClient())
+            {
+                jason = wc.DownloadString(url);
+            }
+
+            return jason;
+        }
+
+
+        /// <summary>
+        /// LoadSuportedcurrency() method will load two drop down menu from CurrencyList.json file. 
+        /// </summary>
+        private void LoadSuportedCurrency()
+        {
+            string jason;
+            using (StreamReader sr = new StreamReader(JCurrencyList))
+            {
+                jason = sr.ReadToEnd();
+            }
+            CurrencyList cl = new CurrencyList();
+            Newtonsoft.Json.JsonConvert.PopulateObject(jason, cl);
+
+            foreach (var item in cl.Currency)
+            {
+                CmbCurrentCurrency.Items.Add(item.Key.ToString());
+                //if (!item.Key.Contains(CmbCurrentCurrency.Text))
+                CmbNewCurrency.Items.Add(item.Key.ToString());
+            }
+        }
+
+
+
+        private void CmbNewCurrency_SelectionChanged(object sender, System.Windows.Controls.SelectionChangedEventArgs e)
+        {
+            TxtResult.Clear();
+
+            string jason;
+            using (StreamReader sr = new StreamReader(JCurrencyList))
+            {
+                jason = sr.ReadToEnd();
+            }
+            CurrencyList cl = new CurrencyList();
+            Newtonsoft.Json.JsonConvert.PopulateObject(jason, cl);
+
+            foreach (var item in cl.Currency)
+            {
+                if (item.Key == CmbCurrentCurrency.SelectedItem.ToString())
+                {
+                    currentCurrency = item.Value.ToString();
+                }
+                if (item.Key == CmbNewCurrency.SelectedItem.ToString())
+                {
+                    newCurrency = item.Value.ToString();
+                }
+            }
+
+            if (CmbCurrentCurrency.Text != "" || CmbNewCurrency.Text != "")
+            {
+                ProcessCurrency();
+            }
+
+        }
+        /// <summary>
+        /// ProcessCurrency() method will load data base on the two selected currency. 
+        /// </summary>
+        private void ProcessCurrency()
+        {
+            string json = GetJasonObjectFromUrl("https://api.fixer.io/latest?base=" + currentCurrency + "");
+
+            CurrencyRates cr = new CurrencyRates();
+
+            Newtonsoft.Json.JsonConvert.PopulateObject(json, cr);
+            foreach (var item in cr.Rates)
+            {
+                if (currentCurrency == newCurrency)
+                {
+                    TxtResult.Text = "1";
+                }
+
+                if (item.Key.ToString() == newCurrency)
+                {
+                    TxtResult.Text = item.Value.ToString();
+                }
+
+
+            }
+        }
+    }
+}
